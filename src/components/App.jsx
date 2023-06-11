d3395fce3f0fef6f6e47efdabcd45f99b9c7b964
ARTEMSHENDRYK <artemshendryk@gmail.com>
2023-06-11 19:52:07 +0300

    Refactoring STATE to HOOK

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,62 +5,59 @@ import Statistics from "./Statistics/Statistics";
 import Notification from "./Notification/Notification";
 import css from "./App.module.css";
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      good: 0,
-      neutral: 0,
-      bad: 0
+function App() {
+  const [good, setGood] = React.useState(0);
+  const [neutral, setNeutral] = React.useState(0);
+  const [bad, setBad] = React.useState(0);
+  
+  const handleIncrement = evt => {
+    switch (evt.target.name) {
+      case "good":
+        setGood(good + 1);
+        break;
+      case "neutral":
+        setNeutral(neutral + 1);
+        break;
+      case "bad":
+        setBad(bad + 1);
+        break;
+      default:
+        break;
     }
-  }  
-
-  handleIncrement = evt => {
-    const { name } = evt.target;      
-    this.setState(prevState => {
-      return { [name]: prevState[name] + 1 }
-    })
   }
 
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
+  const countTotalFeedback = () => {
     return good + neutral + bad;
   }
     
-  countPositiveFeedbackPercentage = () => {
-    const { good } = this.state;
-    return Math.round(good / this.countTotalFeedback() * 100);
+  const countPositiveFeedbackPercentage = () => {
+    return Math.round(good / countTotalFeedback() * 100);
   }
   
-  render() {
-    const { good, neutral, bad } = this.state;
-    const feedbackOptions = Object.keys(this.state);
-
-    return (
-      <div>
-        <h1 className={css.title}>Cafe Expresso</h1>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={feedbackOptions}
-            onLeaveFeedback={this.handleIncrement}
-          />   
-        </Section>
-        <Section title="Ststistics">
-          {this.countTotalFeedback() > 0
-            ? (
-                <Statistics
-                  good={good}
-                  neutral={neutral}
-                  bad={bad}
-                  total={this.countTotalFeedback()}
-                  positivePercentage={this.countPositiveFeedbackPercentage()} />
-            )
-            : (  <Notification message={"There is no feedback"}/> )          
-          }
-         </Section> 
-      </div>
-    )
-  }
-};
+  return (
+    <div>
+      <h1 className={css.title}>Cafe Expresso</h1>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={["good", "neutral", "bad"]}
+          onLeaveFeedback={handleIncrement}
+        />   
+      </Section>
+      <Section title="Ststistics">
+        {countTotalFeedback() > 0
+          ? (
+              <Statistics
+                good={good}
+                neutral={neutral}
+                bad={bad}
+                total={countTotalFeedback()}
+                positivePercentage={countPositiveFeedbackPercentage()} />
+          )
+          : (  <Notification message={"There is no feedback"}/> )          
+        }
+        </Section> 
+    </div>
+  )
+}
 
 export default App;
